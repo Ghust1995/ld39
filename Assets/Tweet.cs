19a01c7abf4326acdb7fad889e8e9066df20aa72
Gustavo Guimaraes <gustavo.ceci95@gmail.com>
2017-07-29 12:54:51 -0300

    added tweets

---
+++ Assets/Tweet.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+[ExecuteInEditMode]
+public class Tweet : MonoBehaviour {
+
+    public Text usernameText;
+    public Text tweetText;
+    public Image iconImage;
+    public Sprite iconSprite;
+    public TwitterApp.Twitter.TweetData tweetData;
+    public string username
+    {
+        get
+        {
+            return tweetData.user;
+        }
+    }
+    public string tweet
+    {
+        get
+        {
+            return tweetData.message;
+        }
+    }
+
+    public void Setup(TwitterApp.Twitter.TweetData tweetData)
+    {
+        this.tweetData = tweetData;
+        iconSprite = Resources.Load<Sprite>("usericons/" + username);
+        usernameText.text = "@" + username;
+        tweetText.text = tweet;
+        iconImage.sprite = iconSprite;
+    }
+	
+}
---
+++ Assets/TwitterApp.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class TwitterApp : MonoBehaviour
+{
+
+    public class Twitter
+    {
+        public class MyTwitterEvent : UnityEvent<TweetData> { }
+
+        public Twitter()
+        {
+            tweetEvent = new MyTwitterEvent();
+        }
+        public MyTwitterEvent tweetEvent;
+        public class TweetData
+        {
+            public string user;
+            public string message;
+            public float time;
+        }
+        float totalTime = 0.0f;
+        float nextTweet = 1.0f;
+        float tweetDelay = 3.0f;
+        public List<TweetData> allTweets = new List<TweetData>();
+
+        public void Update(float dt, List<PokemonInfo> pokemonInformation)
+        {
+            totalTime += dt;
+            if (totalTime > nextTweet)
+            {
+                var newTweet = new TweetData()
+                {
+                    user = "donald",
+                    message = "hello world " + allTweets.Count,
+                    time = totalTime,
+                };
+                tweetEvent.Invoke(newTweet);
+                allTweets.Add(newTweet);
+                nextTweet = totalTime + tweetDelay;
+                Debug.Log("next tweet " + nextTweet);
+            }
+        }
+    }
+
+    public Twitter twitter;
+    public Tweet tweetPrefab;
+    public GameObject tweetContainer;
+    // Use this for initialization
+    void Start()
+    {
+        twitter = new Twitter();
+        twitter.tweetEvent.AddListener(OnNewTweet);
+    }
+
+    private void Update()
+    {
+        twitter.Update(Time.deltaTime, new List<PokemonInfo>());
+    }
+
+    void OnNewTweet(Twitter.TweetData tweetData)
+    {
+        Tweet tweetGO = Instantiate(tweetPrefab, tweetContainer.transform);
+        tweetGO.Setup(tweetData);
+        tweetGO.transform.SetSiblingIndex(0);
+    }
+}
