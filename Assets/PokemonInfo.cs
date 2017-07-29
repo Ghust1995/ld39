bd2db85b74b94fe6d81f05628bacb3154ca6f930
Gustavo Guimaraes <gustavo.ceci95@gmail.com>
2017-07-29 11:15:11 -0300

    Added initial pokemon data

---
+++ Assets/PokemonInfo.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+public class PokemonInfo
+{
+    public MapInfo position;
+    public float GetDistance()
+    {
+        return 10.0f;
+    }
+    public PokemonData data;
+}
+
+public class PokemonData
+{
+    public string name;
+    public int rarity;
+}
+
+public class MapInfo
+{
+    public string streetName1;
+    public string streetName2;
+    public string nearestPOI;
+}
+
+public class MapApp
+{
+
+    public static MapInfo GetMapInfo(Vector2 position)
+    {
+        return new MapInfo()
+        {
+            streetName1 = "st1 name",
+            streetName2 = "st2 name",
+            nearestPOI = "poi"
+        };
+    }
+}
